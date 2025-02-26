c49f34974ecf84eec6dbe3d6e1b33fd1e25007b1
coderepo-deepak <sdpk.ks2@gmail.com>
2025-02-26 14:16:18 +0530

    4th commit

--- main/main.tf
+++ main/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
 
   vpc_cidr             = var.vpc_cidr
   availability_zones   = var.availability_zones
@@ -30,7 +30,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source = "../modules/eks"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
