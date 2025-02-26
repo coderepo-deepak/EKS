99f8a8596c6f50b385c511ee1a734972ff1e0548
coderepo-deepak <sdpk.ks2@gmail.com>
2025-02-26 13:59:03 +0530

    2nd commit

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
-    key            = "terraform.tfstate"
+    bucket         = "my-ds-test"
+    key            = "backend/eks.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
-    encrypt        = true
+    #dynamodb_table = "terraform-eks-state-locks"
+    #encrypt        = true
   }
 }
 
